cfa048f71f81ca7dba1fa5dee65f416dbd65ab6c
Antony Capper <acapper56@gmail.com>
2018-10-25 18:01:17 +0100

    Additional refactoring. Changed model to tank. Implemented two models one for tank top and one for bot

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var Init = function() {
 	var image = loadImage('texture.png');
 	var vsText = loadTextResource('shaders/vert.vert');
 	var fsText = loadTextResource('shaders/frag.frag');
-	//var model = loadJSONResource('models/T34/T34.json');
-	var model = loadJSONResource('models/lowpolydeer/deer.json');
+	var model = loadJSONResource('models/T34/T34.json');
+	//var model = loadJSONResource('models/51tank/m1.json');
+	//var model = loadJSONResource('models/lowpolydeer/deer.json');
 
 	return Promise.all([image, vsText, fsText, model]).then(
 		([imageR, vsTextR, fsTextR, modelR]) => {
@@ -26,7 +27,7 @@ var Init = function() {
 	);
 };
 
-var Run = function(texture, vsText, fsText, model) {
+var Run = function(texture, vsText, fsText, modeljson) {
 	var canvas = document.getElementById('mycanvas');
 	var gl = canvas.getContext('webgl');
 
@@ -47,11 +48,15 @@ var Run = function(texture, vsText, fsText, model) {
 
 	var shader = new Shader(gl, vsText, fsText);
 
-	var s = [0.009, 0.009, 0.009];
-	var r = [270, 0, 50];
-	var t = [0, 0, -500];
+	var s = [0.006, 0.006, 0.006];
+	var r = [0, 90, 0];
+	var t = [0, 0, 0];
 
-	var model = new Model(gl, model, texture, shader, s, r, t);
+	var tankTop = [0, 2, 3, 7, 9, 10, 13];
+	var tankBot = [1, 4, 5, 6, 8, 11, 12];
+
+	var modeltop = new Model(gl, modeljson, texture, shader, s, r, t, tankTop);
+	var modelbot = new Model(gl, modeljson, texture, shader, s, r, t, tankBot);
 
 	const fpsElem = document.querySelector('#fps');
 
@@ -60,18 +65,16 @@ var Run = function(texture, vsText, fsText, model) {
 	var proj = new Float32Array(16);
 
 	mat4.identity(world);
-	mat4.lookAt(view, [0, 8, -10], [0, 4, -2], [0, 1, 0]);
+	mat4.lookAt(view, [0, 2, -3], [0, 0, 0], [0, 1, 0]);
 	mat4.perspective(proj, glMatrix.toRadian(90), 1280 / 960, 0.1, 1000.0);
 
-	var mShaderProgram = model.getShader().getProgram();
+	var mShaderProgram = modeltop.getShader().getProgram();
 	gl.useProgram(mShaderProgram);
 
-	var worldL = gl.getUniformLocation(mShaderProgram, 'world');
-	var viewL = gl.getUniformLocation(mShaderProgram, 'view');
+	var mvmatrixL = gl.getUniformLocation(mShaderProgram, 'mvmatrix');
 	var projL = gl.getUniformLocation(mShaderProgram, 'proj');
 
-	gl.uniformMatrix4fv(worldL, gl.FALSE, world);
-	gl.uniformMatrix4fv(viewL, gl.FALSE, view);
+	gl.uniformMatrix4fv(mvmatrixL, gl.FALSE, mat4.mul(view, world, view));
 	gl.uniformMatrix4fv(projL, gl.FALSE, proj);
 
 	var lightL = gl.getUniformLocation(mShaderProgram, 'light');
@@ -83,16 +86,69 @@ var Run = function(texture, vsText, fsText, model) {
 	gl.uniform3f(lightPosL, 0.0, 10.0, -10.0);
 
 	var then = 0;
-	var loop = function(now) {
+	var draw = function(now) {
 		now *= 0.001; // convert to seconds
 		const deltaTime = now - then; // compute time since last frame
 		then = now; // remember time for next frame
 		const fps = 1 / deltaTime; // compute frames per second
 		fpsElem.textContent = fps.toFixed(1); // update fps display
 
-		model.draw(performance.now(), world, view);
-
-		requestAnimationFrame(loop);
+		update(deltaTime);
+		render(deltaTime);
+		requestAnimationFrame(draw);
 	};
-	requestAnimationFrame(loop);
+	requestAnimationFrame(draw);
+
+	var render = function(delta) {
+		gl.clearColor(0.1, 0.1, 0.1, 1);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		modeltop.draw(world, view);
+		modelbot.draw(world, view);
+	};
+
+	var update = function(delta) {
+		keyboard();
+		modeltop.update(delta);
+		modelbot.update(delta);
+	};
+
+	var keyboard = function(delta) {
+		if (keys['E'.charCodeAt(0)]) {
+			var rot = modeltop.getRotation();
+			rot[1] -= 1;
+			modeltop.setRotation(rot);
+		}
+		if (keys['Q'.charCodeAt(0)]) {
+			var rot = modeltop.getRotation();
+			rot[1] += 1;
+			modeltop.setRotation(rot);
+		}
+		if (keys['D'.charCodeAt(0)]) {
+			var rot = modelbot.getRotation();
+			rot[1] -= 0.5;
+			modelbot.setRotation(rot);
+		}
+		if (keys['A'.charCodeAt(0)]) {
+			var rot = modelbot.getRotation();
+			rot[1] += 0.5;
+			modelbot.setRotation(rot);
+		}
+	};
 };
+
+var keys = [];
+window.addEventListener(
+	'keydown',
+	function(e) {
+		keys[e.keyCode] = true;
+	},
+	false
+);
+
+window.addEventListener(
+	'keyup',
+	function(e) {
+		keys[e.keyCode] = false;
+	},
+	false
+);
--- model.js
+++ model.js
@@ -1,41 +1,65 @@
 class Model {
-	constructor(gl, modeljson, texture, shader, scale, rotation, translation) {
+	constructor(
+		gl,
+		modeljson,
+		texture,
+		shader,
+		scale,
+		rotation,
+		translation,
+		meshIndices
+	) {
 		this.gl = gl;
 
 		this.modeljson = modeljson;
 		this.texture = texture;
+		this.meshIndices = meshIndices;
 
 		this.shader = shader;
 
-		this.indicesLength = 0;
-		this.indexBuffer = null;
+		this.indicesLength = [];
+		this.indexBuffer = [];
 		this.buffers = [];
-
-		this.angle = 0;
+		this.bufferKeys = [
+			{
+				key: 'position',
+				numOfEls: 3,
+				size: 3,
+				offset: 0
+			},
+			{
+				key: 'texture',
+				numOfEls: 2,
+				size: 2,
+				offset: 0
+			},
+			{
+				key: 'normal',
+				numOfEls: 3,
+				size: 3,
+				offset: 0
+			}
+		];
 
 		this.identityMat = new Float32Array(16);
 		mat4.identity(this.identityMat);
 
 		// Static transforms
 		// TO DO Combine xrot yrot zrot into vector
-		this.fxRot = glMatrix.toRadian(rotation[0]);
-		this.fxRotMat = new Float32Array(16);
-		this.fyRot = glMatrix.toRadian(rotation[1]);
-		this.fyRotMat = new Float32Array(16);
-		this.fzRot = glMatrix.toRadian(rotation[2]);
-		this.fzRotMat = new Float32Array(16);
+		this.fRot = rotation;
 		this.fRotMat = new Float32Array(16);
 
 		this.fscale = scale;
 		this.fScaleMat = new Float32Array(16);
 		this.ftrans = translation;
 		this.fTransMat = new Float32Array(16);
+		this.rxm = mat4.create();
+		this.rym = mat4.create();
+		this.rzm = mat4.create();
 
 		// Dynamic transforms
 		// TO DO Combine xrot yrot zrot into vector
-		this.xRot = new Float32Array(16);
-		this.yRot = new Float32Array(16);
-		this.zRot = new Float32Array(16);
+		this.rot = [0, 0, 0];
 		this.rotMat = new Float32Array(16);
 
 		this.scale = [1, 1, 1];
@@ -69,37 +93,54 @@ class Model {
 	}
 
 	createBuffers() {
-		var modelIndicies = [].concat.apply([], this.modeljson.meshes[0].faces);
-		var modelVerts = this.modeljson.meshes[0].vertices;
-		var modelTextureCoords = this.modeljson.meshes[0].texturecoords[0];
-		var modelNormals = this.modeljson.meshes[0].normals;
+		var i = 0;
+		this.modeljson.meshes.forEach(e => {
+			if (this.meshIndices == null || this.meshIndices.indexOf(i) > -1) {
+				var modelIndicies = [].concat.apply([], e.faces);
+				var modelVerts = e.vertices;
+				var modelTextureCoords = e.texturecoords[0];
+				var modelNormals = e.normals;
 
-		this.indicesLength = modelIndicies.length;
-		this.indexBuffer = this.gl.createBuffer();
-		this.bindBufferData(
-			this.indexBuffer,
-			new Uint16Array(modelIndicies),
-			this.gl.ELEMENT_ARRAY_BUFFER
-		);
+				this.indicesLength.push(modelIndicies.length);
+				this.createNewIndexBuffer();
+				this.bindBufferData(
+					this.getLastIndexBuffer(),
+					new Uint16Array(modelIndicies),
+					this.gl.ELEMENT_ARRAY_BUFFER
+				);
 
-		this.createNewBuffer();
-		this.bindBufferData(
-			this.getLastBuffer(),
-			new Float32Array(modelVerts),
-			this.gl.ARRAY_BUFFER
-		);
-		this.createNewBuffer();
-		this.bindBufferData(
-			this.getLastBuffer(),
-			new Float32Array(modelTextureCoords),
-			this.gl.ARRAY_BUFFER
-		);
-		this.createNewBuffer();
-		this.bindBufferData(
-			this.getLastBuffer(),
-			new Float32Array(modelNormals),
-			this.gl.ARRAY_BUFFER
-		);
+				this.createNewBuffer();
+				this.bindBufferData(
+					this.getLastBuffer(),
+					new Float32Array(modelVerts),
+					this.gl.ARRAY_BUFFER
+				);
+				this.createNewBuffer();
+				this.bindBufferData(
+					this.getLastBuffer(),
+					new Float32Array(modelTextureCoords),
+					this.gl.ARRAY_BUFFER
+				);
+				this.createNewBuffer();
+				this.bindBufferData(
+					this.getLastBuffer(),
+					new Float32Array(modelNormals),
+					this.gl.ARRAY_BUFFER
+				);
+			}
+			i++;
+		});
+	}
+
+	setRotation(rot) {
+		this.rot = rot;
+	}
+	getRotation() {
+		return this.rot;
+	}
+
+	createNewIndexBuffer() {
+		this.indexBuffer.push(this.gl.createBuffer());
 	}
 
 	createNewBuffer() {
@@ -108,23 +149,36 @@ class Model {
 
 	bindBufferData(bufferL, data, type) {
 		this.gl.bindBuffer(type, bufferL);
-		this.gl.bufferData(type, data, this.gl.STATIC_DRAW);
+		this.gl.bufferData(type, data, this.gl.DYNAMIC_DRAW);
 	}
 
 	getLastBuffer() {
 		return this.buffers[this.buffers.length - 1];
 	}
 
-	bindArrayBuffers() {
-		var pL = this.gl.getAttribLocation(
-			this.shader.getProgram(),
-			'position'
-		);
-		this.bindArrayBuffer(this.buffers[0], pL, 3, 3, 0);
-		var tL = this.gl.getAttribLocation(this.shader.getProgram(), 'texture');
-		this.bindArrayBuffer(this.buffers[1], tL, 2, 2, 0);
-		var nL = this.gl.getAttribLocation(this.shader.getProgram(), 'normal');
-		this.bindArrayBuffer(this.buffers[2], nL, 3, 3, 0);
+	getLastIndexBuffer() {
+		return this.indexBuffer[this.indexBuffer.length - 1];
+	}
+
+	bindIndexBuffers(i) {
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer[i]);
+	}
+
+	bindArrayBuffers(i) {
+		var program = this.shader.getProgram();
+		var j = 0;
+
+		this.bufferKeys.forEach(e => {
+			var location = this.gl.getAttribLocation(program, e.key);
+			this.bindArrayBuffer(
+				this.buffers[i * this.bufferKeys.length + j],
+				location,
+				e.numOfEls,
+				e.size,
+				e.offset
+			);
+			j++;
+		});
 	}
 
 	bindArrayBuffer(vbo, attribL, numOfEls, size, offset) {
@@ -135,7 +189,7 @@ class Model {
 			this.gl.FLOAT, // Type of elements
 			this.gl.FALSE,
 			size * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-			offset // Offset from the beginning of a single vertex to this attribute
+			offset * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
 		);
 		this.gl.enableVertexAttribArray(attribL);
 	}
@@ -183,14 +237,99 @@ class Model {
 		this.gl.uniformMatrix3fv(this.nmatrixL, this.gl.FALSE, this.nmatrix3);
 	}
 
-	rotateMat(mat, rxm, rym, rzm, rxa, rya, rza) {
-		mat4.fromRotation(rxm, rxa, [1, 0, 0]);
-		mat4.fromRotation(rym, rya, [0, 1, 0]);
-		mat4.fromRotation(rzm, rza, [0, 0, 1]);
+	draw(world, view) {
+		this.bindTexture();
+		for (var i = 0; i < this.indexBuffer.length; i++) {
+			this.gl.uniformMatrix4fv(this.modelL, this.gl.FALSE, this.model);
+
+			mat4.identity(this.nmatrix4);
+			mat4.mul(this.nmatrix4, this.nmatrix4, view);
+			mat4.mul(this.nmatrix4, this.nmatrix4, world);
+			mat4.mul(this.nmatrix4, this.nmatrix4, this.model);
+
+			mat3.fromMat4(this.nmatrix3, this.nmatrix4);
+			mat3.invert(this.nmatrix3, this.nmatrix3);
+			mat3.transpose(this.nmatrix3, this.nmatrix3);
+
+			this.bindArrayBuffers(i);
+			this.bindIndexBuffers(i);
+			this.bindUniforms();
+
+			this.gl.drawElements(
+				this.gl.TRIANGLES,
+				this.indicesLength[i],
+				this.gl.UNSIGNED_SHORT,
+				0
+			);
+		}
+	}
+
+	update(delta) {
+		this.transform(delta);
+	}
+
+	transform(delta) {
+		mat4.mul(this.model, this.identityMat, this.identityMat);
+
+		this.staticTransform();
+		this.dynamicTransform();
+	}
+
+	staticTransform() {
+		this.model = this.transformModel(
+			this.model,
+			this.fscale,
+			this.fScaleMat,
+			this.fRot,
+			this.fRotMat,
+			this.ftrans,
+			this.fTransMat
+		);
+	}
+
+	dynamicTransform() {
+		this.model = this.transformModel(
+			this.model,
+			this.scale,
+			this.scaleMat,
+			this.rot,
+			this.rotMat,
+			this.trans,
+			this.transMat
+		);
+	}
+
+	transformModel(model, scale, scaleMat, rot, rotMat, trans, transMat) {
+		// Scale
+		mat4.fromScaling(scaleMat, scale);
+		mat4.mul(model, model, scaleMat);
+
+		// Rotate
+		rotMat = this.rotateMat(
+			rotMat,
+			glMatrix.toRadian(rot[0]),
+			glMatrix.toRadian(rot[1]),
+			glMatrix.toRadian(rot[2])
+		);
+		mat4.mul(model, model, rotMat);
+
+		// Translate
+		mat4.fromTranslation(transMat, trans);
+		mat4.mul(model, model, transMat);
+		return model;
+	}
+
+	rotateMat(mat, rxa, rya, rza) {
+		mat4.mul(this.rxm, this.identityMat, this.identityMat);
+		mat4.mul(this.rym, this.identityMat, this.identityMat);
+		mat4.mul(this.rzm, this.identityMat, this.identityMat);
+		mat4.fromRotation(this.rxm, rxa, [1, 0, 0]);
+		mat4.fromRotation(this.rym, rya, [0, 1, 0]);
+		mat4.fromRotation(this.rzm, rza, [0, 0, 1]);
 		mat4.mul(mat, this.identityMat, this.identityMat);
-		mat4.mul(mat, mat, rxm);
-		mat4.mul(mat, mat, rym);
-		mat4.mul(mat, mat, rzm);
+		mat4.mul(mat, mat, this.rxm);
+		mat4.mul(mat, mat, this.rym);
+		mat4.mul(mat, mat, this.rzm);
 		return mat;
 	}
 
@@ -204,89 +343,5 @@ class Model {
 
 	rotateZ(angle, mat) {
 		return mat4.rotate(mat, this.identityMat, angle, [0, 0, 1]);
-	}
-
-	draw(delta, world, view) {
-		this.transformModel(delta);
-
-		mat4.identity(this.nmatrix4);
-		mat4.mul(this.nmatrix4, this.nmatrix4, view);
-		mat4.mul(this.nmatrix4, this.nmatrix4, world);
-		mat4.mul(this.nmatrix4, this.nmatrix4, this.model);
-
-		mat3.fromMat4(this.nmatrix3, this.nmatrix4);
-		mat3.invert(this.nmatrix3, this.nmatrix3);
-		mat3.transpose(this.nmatrix3, this.nmatrix3);
-
-		this.bindArrayBuffers();
-		this.bindUniforms();
-
-		this.gl.clearColor(0.1, 0.1, 0.1, 1);
-		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-		this.gl.uniformMatrix4fv(this.modelL, this.gl.FALSE, this.model);
-
-		//this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
-		//this.gl.activeTexture(this.gl.TEXTURE0);
-		this.bindTexture();
-
-		this.gl.drawElements(
-			this.gl.TRIANGLES,
-			this.indicesLength,
-			this.gl.UNSIGNED_SHORT,
-			0
-		);
-	}
-
-	transformModel(delta) {
-		mat4.mul(this.model, this.identityMat, this.identityMat);
-
-		this.angle = ((delta / 1000) * (2 * Math.PI)) / 4;
-
-		this.staticTransform();
-		this.dynamicTransform();
-	}
-
-	staticTransform() {
-		// Scale
-		mat4.fromScaling(this.fScaleMat, this.fscale);
-		mat4.mul(this.model, this.model, this.fScaleMat);
-
-		// Rotate
-		this.fRotMat = this.rotateMat(
-			this.fRotMat,
-			this.fxRotMat,
-			this.fyRotMat,
-			this.fzRotMat,
-			this.fxRot,
-			this.fyRot,
-			this.fzRot
-		);
-		mat4.mul(this.model, this.model, this.fRotMat);
-
-		// Translate
-		mat4.fromTranslation(this.fTransMat, this.ftrans);
-		mat4.mul(this.model, this.model, this.fTransMat);
-	}
-
-	dynamicTransform() {
-		// Scale
-		mat4.fromScaling(this.scaleMat, this.scale);
-		mat4.mul(this.model, this.model, this.scaleMat);
-
-		// Rotate
-		this.rotMat = this.rotateMat(
-			this.rotMat,
-			this.xRot,
-			this.yRot,
-			this.zRot,
-			0,
-			0,
-			this.angle / 6
-		);
-		mat4.mul(this.model, this.model, this.rotMat);
-
-		// Translate
-		mat4.fromTranslation(this.transMat, this.trans);
-		mat4.mul(this.model, this.model, this.transMat);
 	}
 }
