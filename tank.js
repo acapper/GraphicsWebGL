4739b05b8e07c2b4240608877fd893d3a2c71747
Antony Capper <acapper56@gmail.com>
2018-10-31 23:02:54 +0000

    Rewrote entire tank class to make it easier to interact with

--- tank.js
+++ tank.js
@@ -1,207 +1,232 @@
 class Tank {
-	constructor(top, bot, bomb) {
-		this.modeltop = new Model(top);
-		this.modelbot = new Model(bot);
-		this.bomb = new Light(bomb[0], bomb[1]);
+	constructor(model) {
+		/*{
+		gl,
+		texture: texture,
+		model: modeljson,
+		shader: shader,
+		rotation: [0, 180, 0],
+		scale: [0.006, 0.006, 0.006],
+		translation: [0, 0, 0],
+		topIndices: [0],
+		botIndices: [1]
+	}*/
+		this.top = new Model({
+			gl: model.gl,
+			texture: model.texture,
+			model: model.model,
+			shader: model.shader,
+			rotation: model.top.rotation,
+			scale: model.top.scale,
+			translation: model.top.translation,
+			meshIndices: model.top.indices
+		});
+		this.bot = new Model({
+			gl: model.gl,
+			texture: model.texture,
+			model: model.model,
+			shader: model.shader,
+			rotation: model.bot.rotation,
+			scale: model.bot.scale,
+			translation: model.bot.translation,
+			meshIndices: model.bot.indices
+		});
+		this.bomb = new Light(
+			{
+				gl: model.gl,
+				texture: model.bomb.texture,
+				model: model.bomb.model,
+				shader: model.shader,
+				rotation: model.bomb.rotation,
+				scale: model.bomb.scale,
+				translation: model.bomb.translation
+			},
+			model.bomb.light
+		);
 
-		this.v = 75;
-		this.rottop = this.modeltop.getRotation();
-		this.rotbot = this.modelbot.getRotation();
-		this.rotf = this.modeltop.getRotationF();
-		this.transtop = this.modeltop.getTrans();
-		this.transbot = this.modelbot.getTrans();
-		this.temp = vec3.create();
-		this.forward = [
-			Math.sin(glMatrix.toRadian(this.rottop[1] + this.rotf[1])),
-			0,
-			Math.cos(glMatrix.toRadian(this.rottop[1] + this.rotf[1]))
-		];
-		this.d = 0.15;
+		this.camera = new Camera({
+			viewPos: [
+				model.top.translation[0] + model.camera.position[0],
+				model.top.translation[1] + model.camera.position[1],
+				model.top.translation[2] + model.camera.position[2]
+			],
+			viewLook: model.top.translation,
+			viewUp: [0, 1, 0]
+		});
+		this.camera.rotateViewPos(model.top.rotation);
+
+		this.turrentSpeed = 60;
+		this.tankRotSpeed = 45;
+		this.tankSpeed = 0.4;
 		this.bombVisible = false;
-		this.bombMoving = false;
 		this.bombDirection = [0, 0, 0];
-		this.bombTrans = this.bomb.getModel().getTrans();
-		this.bombSpeed = 10;
-		//this.bombSpeed = 0;
-		top = null;
-		bot = null;
-		bomb = null;
+		this.fired = false;
+		this.bombSpeed = 50;
+		this.bombPos = this.bomb.getTranslation();
+		this.bombRot = this.bomb.getRotation();
 	}
 
-	getPosition() {
-		return this.modelbot.getTrans();
+	getCamera() {
+		return this.camera;
 	}
 
-	getRotation() {
-		return this.modelbot.getRotation();
+	setCamera(camera) {
+		this.camera = camera;
 	}
 
 	getBomb() {
 		return this.bomb;
 	}
 
-	keyboardInput(delta, keys) {
-		this.move(delta, keys);
+	getTranslation() {
+		return this.bot.getTranslation();
+	}
+
+	getRotation() {
+		return this.bot.getRotation();
+	}
+
+	draw(gl, world, view, proj, lights) {
+		this.top.draw(gl, world, view, proj, lights);
+		this.bot.draw(gl, world, view, proj, lights);
+		if (this.bombVisible) this.bomb.draw(gl, world, view, proj, lights);
+	}
+
+	update(delta, keys) {
+		this.input(delta, keys);
+		this.top.update();
+		this.bot.update();
+		this.bomb.update();
+		if (this.fired) {
+			var trans = this.bomb.getTranslation();
+			this.bomb.setTranslation([
+				trans[0] + delta * this.bombSpeed * this.bombDirection[0],
+				trans[1],
+				trans[2] + delta * this.bombSpeed * this.bombDirection[2]
+			]);
+		}
+	}
+
+	input(delta, keys) {
 		this.rotate(delta, keys);
-		this.fire(keys);
-		keys = null;
-		delta = null;
+		this.move(delta, keys);
 	}
 
 	rotate(delta, keys) {
 		if (keys['E'.charCodeAt(0)]) {
-			this.rotateTop(delta, -this.v, [0, 1, 0]);
+			this.rotateModel(delta, -this.turrentSpeed, this.top);
+			this.rotateBomb(delta, -this.turrentSpeed);
 		}
 		if (keys['Q'.charCodeAt(0)]) {
-			this.rotateTop(delta, this.v, [0, 1, 0]);
+			this.rotateModel(delta, this.turrentSpeed, this.top);
+			this.rotateBomb(delta, this.turrentSpeed);
 		}
 		if (keys['D'.charCodeAt(0)]) {
-			this.rotateTopAndBot(delta, -this.v * 0.5, [0, 1, 0]);
+			this.rotateModel(delta, -this.tankRotSpeed, this.top);
+			this.rotateModel(delta, -this.tankRotSpeed, this.bot);
+			this.rotateBomb(delta, -this.tankRotSpeed);
+			this.camera.rotateViewPos([0, delta * -this.tankRotSpeed, 0]);
 		}
 		if (keys['A'.charCodeAt(0)]) {
-			this.rotateTopAndBot(delta, this.v * 0.5, [0, 1, 0]);
+			this.rotateModel(delta, this.tankRotSpeed, this.top);
+			this.rotateModel(delta, this.tankRotSpeed, this.bot);
+			this.rotateBomb(delta, this.tankRotSpeed);
+			this.camera.rotateViewPos([0, delta * this.tankRotSpeed, 0]);
 		}
-		keys = null;
-		delta = null;
 	}
 
-	rotateTop(delta, v, axis) {
-		this.rottop = this.modeltop.getRotation();
-		this.rottop = this.calcRotation(axis, v, delta, this.rottop);
-		this.modeltop.setRotation(this.rottop);
-		axis = null;
-		delta = null;
-		v = null;
+	rotateBomb(delta, speed) {
+		var tankPos = this.top.getTranslation();
+		vec3.rotateY(
+			this.bombPos,
+			this.bombPos,
+			tankPos,
+			glMatrix.toRadian(speed * delta)
+		);
+
+		this.bombRot[1] = this.bombRot[1] + speed * delta;
+		if (!this.fired) {
+			this.bomb.setRotation([
+				this.bombRot[0],
+				this.bombRot[1],
+				this.bombRot[2]
+			]);
+			this.bomb.setTranslation([
+				this.bombPos[0],
+				this.bombPos[1],
+				this.bombPos[2]
+			]);
+		}
 	}
 
-	rotateBot(delta, v, axis) {
-		this.rotbot = this.modelbot.getRotation();
-		this.rotbot = this.calcRotation(axis, v, delta, this.rotbot);
-		this.modelbot.setRotation(this.rotbot);
-		axis = null;
-		delta = null;
-		v = null;
-	}
-
-	rotateTopAndBot(delta, v, axis) {
-		this.rotateTop(delta, v, axis);
-		this.rotateBot(delta, v, axis);
-		axis = null;
-		delta = null;
-	}
-
-	calcRotation(axis, v, delta, rot) {
-		axis.forEach(function(e, index) {
-			if (e == 1) {
-				rot[index] += v * delta;
-			}
-		});
-		axis = null;
-		v = null;
-		delta = null;
-		return rot;
+	rotateModel(delta, speed, model) {
+		var rot = model.getRotation();
+		rot[1] = rot[1] + speed * delta;
+		model.setRotation(rot);
 	}
 
 	move(delta, keys) {
 		if (keys['W'.charCodeAt(0)]) {
-			this.moveForward(delta * this.d);
+			this.moveModel(delta * this.tankSpeed);
 		}
 		if (keys['S'.charCodeAt(0)]) {
-			this.moveForward(delta * -this.d);
+			this.moveModel(delta * -this.tankSpeed);
 		}
-		delta = null;
-		keys = null;
-	}
-
-	moveForward(d) {
-		this.rot = this.modelbot.getRotation();
-		this.rotf = this.modelbot.getRotationF();
-		this.forward = [
-			Math.sin(glMatrix.toRadian(this.rot[1] + this.rotf[1])),
-			0,
-			Math.cos(glMatrix.toRadian(this.rot[1] + this.rotf[1]))
-		];
-		vec3.normalize(this.forward, this.forward);
-		this.transbot = this.modelbot.getTrans();
-		this.transtop = this.modeltop.getTrans();
-		this.temp = vec3.create();
-		vec3.mul(this.temp, [d, d, d], this.forward);
-		vec3.add(this.transbot, this.transbot, this.temp);
-		this.modelbot.setTrans(this.transbot);
-		vec3.add(this.transtop, this.transtop, this.temp);
-		this.modelbot.setTrans(this.transtop);
-		d = null;
-	}
-
-	fire(keys) {
-		if (!this.bombVisible) {
-			if (keys['V'.charCodeAt(0)] && !this.bombVisible) {
-				this.transtop = this.modeltop.getTrans();
-				this.rot = this.modeltop.getRotation();
-				this.rotf = this.modeltop.getRotationF();
-				this.forward = [
-					Math.sin(glMatrix.toRadian(this.rot[1] + this.rotf[1])),
-					1,
-					Math.cos(glMatrix.toRadian(this.rot[1] + this.rotf[1]))
-				];
-				this.bomb
-					.getModel()
-					.setTrans([
-						this.transtop[0] + 3 * this.forward[0] - 2.87,
-						1.36,
-						this.transtop[2] + 3 * this.forward[2]
-					]);
-				this.bomb.getModel().setRotation([-this.rot[1], 0, 0]);
-				this.bombVisible = true;
-
-				var that = this;
-				this.bombDirection = this.forward;
-				this.bombMoving = true;
-				this.bomb.setOn(1);
-				setTimeout(function() {
-					that.getBomb().setOn(0);
-					that.hideBomb();
-					that = null;
-				}, 1500);
-			}
-		}
-		keys = null;
-	}
-
-	hideBomb() {
-		this.bombVisible = false;
-		this.bombMoving = false;
-	}
-
-	draw(gl, world, view, proj, lights) {
-		this.modeltop.draw(gl, world, view, proj, lights);
-		this.modelbot.draw(gl, world, view, proj, lights);
-		if (this.bombVisible) {
-			this.bomb.draw(gl, world, view, proj, lights);
-		}
-		gl = null;
-		world = null;
-		view = null;
-		proj = null;
-		lights = null;
-	}
-
-	update(delta) {
-		this.modeltop.update(delta);
-		this.modelbot.update(delta);
-		this.bomb.getModel().update(delta);
-		if (this.bombMoving) {
-			this.bombTrans = this.bomb.getModel().getTrans();
-			this.bomb
-				.getModel()
-				.setTrans([
-					this.bombTrans[0] +
-						delta * this.bombSpeed * this.bombDirection[0],
-					this.bombTrans[1],
-					this.bombTrans[2] +
-						delta * this.bombSpeed * this.bombDirection[2]
+		if (keys['V'.charCodeAt(0)] && !this.fired) {
+			var rot = this.bomb.getRotation();
+			var forward = [
+				Math.cos(glMatrix.toRadian(rot[1])),
+				0,
+				-Math.sin(glMatrix.toRadian(rot[1]))
+			];
+			vec3.normalize(forward, forward);
+			vec3.mul(
+				forward,
+				[this.tankSpeed, this.tankSpeed, this.tankSpeed],
+				forward
+			);
+			this.bombDirection = forward;
+			this.fired = true;
+			this.bombVisible = true;
+			var that = this;
+			this.bomb.setOn(1);
+			setTimeout(function() {
+				that.bomb.setOn(0);
+				that.bomb.setRotation([
+					that.bombRot[0],
+					that.bombRot[1],
+					that.bombRot[2]
 				]);
+				that.bomb.setTranslation([
+					that.bombPos[0],
+					that.bombPos[1],
+					that.bombPos[2]
+				]);
+				that.fired = false;
+				that.bombVisible = false;
+				that = null;
+			}, 1500);
 		}
-		delta = null;
+	}
+
+	moveModel(speed) {
+		var rot = this.bot.getRotation();
+		var forward = [
+			Math.sin(glMatrix.toRadian(rot[1])),
+			0,
+			Math.cos(glMatrix.toRadian(rot[1]))
+		];
+		vec3.normalize(forward, forward);
+		vec3.mul(forward, [speed, speed, speed], forward);
+		this.translateModel(forward, this.top);
+		this.translateModel(forward, this.bot);
+		this.camera.transCamera(forward);
+		vec3.add(this.bombPos, this.bombPos, forward);
+	}
+
+	translateModel(forward, model) {
+		var trans = model.getTranslation();
+		vec3.add(trans, trans, forward);
+		model.setTranslation(trans);
 	}
 }
