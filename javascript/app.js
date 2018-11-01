8ec19ebdc938da8cc28b53d28f16b799a906aedc
Antony Capper <acapper56@gmail.com>
2018-11-01 18:42:28 +0000

    Project folder restructure

---
+++ javascript/app.js
@@ -0,0 +1,397 @@
+const canvas = document.getElementById('mycanvas');
+const fpsElem = document.querySelector('#fps');
+
+var world = new Float32Array(16);
+var view = new Float32Array(16);
+var proj = new Float32Array(16);
+
+var viewPos = [0, 3, -5];
+
+var lights = [];
+var lightsJSON;
+
+var light;
+var tank;
+var deer1;
+var deer2;
+var plane;
+
+var camera;
+var lightMove;
+
+var keys = [];
+var gl;
+
+// Initialise resources needed for opengl scene
+var Init = function() {
+	// Load external resources
+	var proms = loadAll([
+		'textures/texture4.png',
+		'textures/texture2.png',
+		'textures/texture3.png',
+		'shaders/vert.vert',
+		'shaders/frag.frag',
+		'shaders/light.frag',
+		'models/t34.json',
+		'models/deer.json',
+		'models/sphere.json',
+		'models/tankshell.json',
+		'models/plane.json',
+		'textures/texture.png'
+	]);
+
+	// Wait for external resources to load
+	Promise.all(proms)
+		// External resources have loaded
+		.then(promsR => {
+			// Get webgl package
+			gl = canvas.getContext('webgl');
+
+			// If cant get standard webgl package use experimental
+			if (!gl) {
+				gl = canvas.getContext('experimental-webgl');
+			}
+
+			// If still cant get webgl package browser doesn't support
+			if (!gl) {
+				alert('Your browser does not support WebGL');
+			}
+
+			// Make sure html checkboxes start in correct state
+			$("[name='blueLight']").prop('checked', true);
+			$("[name='blueLightMove']").prop('checked', false);
+			$("[name='redLight']").prop('checked', true);
+			$("[name='redLightMove']").prop('checked', false);
+
+			// Set gl statemachine values
+			gl.clearColor(0, 0, 0, 1);
+			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+			gl.enable(gl.DEPTH_TEST);
+			gl.enable(gl.CULL_FACE);
+			gl.frontFace(gl.CCW);
+			gl.cullFace(gl.BACK);
+
+			// Load and create shaders
+			var shader = new Shader(gl, promsR[3], promsR[4]);
+			var lightshader = new Shader(gl, promsR[3], promsR[5]);
+
+			// Run gl scene
+			Run(
+				promsR[0],
+				promsR[1],
+				promsR[2],
+				shader,
+				lightshader,
+				promsR[6],
+				promsR[7],
+				promsR[8],
+				promsR[9],
+				promsR[10],
+				promsR[11]
+			);
+		})
+		// Catch any errors once the external resources have loaded
+		.catch(err => {
+			throw err;
+		});
+};
+
+// Create and run gl scene
+var Run = function(
+	whiteTexture,
+	tankTexture,
+	bulletTexture,
+	shader,
+	lightshader,
+	tankjson,
+	deerjson,
+	spherejson,
+	bulletjson,
+	planejson,
+	pinkTexture
+) {
+	// Create deer models
+	deer1 = new Model({
+		gl,
+		texture: pinkTexture,
+		model: deerjson,
+		shader: shader,
+		rotation: [-90, 0, 0],
+		scale: [0.002, 0.002, 0.002],
+		translation: [5, 0, 0]
+	});
+	deer2 = new Model({
+		gl,
+		texture: pinkTexture,
+		model: deerjson,
+		shader: shader,
+		rotation: [-90, 0, 0],
+		scale: [0.002, 0.002, 0.002],
+		translation: [-5, 0, 0]
+	});
+	plane = new Model({
+		gl,
+		texture: whiteTexture,
+		model: planejson,
+		shader: shader,
+		rotation: [0, 0, 0],
+		scale: [100, 100, 100],
+		translation: [0, 0, 0]
+	});
+
+	// Create tank model
+	tank = new Tank({
+		gl,
+		texture: tankTexture,
+		model: tankjson,
+		shader: shader,
+		top: {
+			rotation: [0, 0, 0],
+			scale: [0.006, 0.006, 0.006],
+			translation: [0, 0, -3],
+			indices: [0, 2, 3, 7, 9, 10, 13]
+		},
+		bot: {
+			rotation: [0, 0, 0],
+			scale: [0.006, 0.006, 0.006],
+			translation: [0, 0, -3],
+			indices: [1, 4, 5, 6, 8, 11, 12, 14]
+		},
+		bomb: {
+			texture: bulletTexture,
+			model: bulletjson,
+			rotation: [0, -90, -90],
+			scale: [0.05, 0.05, 0.05],
+			translation: [0, 1.36, 0.2],
+			light: {
+				name: 'bomb',
+				colour: [0.6, 0.3, 0],
+				direction: [0, 0, 0],
+				on: 0
+			}
+		},
+		camera: {
+			position: viewPos
+		}
+	});
+
+	// Create red light
+	light = new Light(
+		{
+			gl,
+			texture: whiteTexture,
+			model: spherejson,
+			shader: lightshader,
+			rotation: [0, 0, 0],
+			scale: [0.25, 0.25, 0.25],
+			translation: [2, 3, 0]
+		},
+		{ name: 'red', colour: [1, 0, 0], direction: [0, 0, 0], on: 1 }
+	);
+	// Add light to light tracker
+	lights.push(light);
+
+	// Create blue light
+	light = new Light(
+		{
+			gl,
+			texture: whiteTexture,
+			model: spherejson,
+			shader: lightshader,
+			rotation: [0, 0, 0],
+			scale: [0.25, 0.25, 0.25],
+			translation: [-2, 3, 0]
+		},
+		{ name: 'blue', colour: [0, 0, 1], direction: [0, 0, 0], on: 1 }
+	);
+	// Add light to light tracker
+	lights.push(light);
+
+	// Add tank bullet light to light tracker
+	lights.push(tank.getBomb());
+
+	// Create world, view and project matrix
+	mat4.identity(world);
+	view = tank.getCamera().getCameraMat();
+	mat4.perspective(
+		proj,
+		glMatrix.toRadian(90),
+		canvas.clientWidth / canvas.clientHeight,
+		0.1,
+		10000.0
+	);
+
+	// Start draw loop
+	requestAnimationFrame(draw);
+};
+
+// Web gl main draw loop
+var then = 0; // Keep track of last frame time
+var draw = function(now) {
+	now *= 0.001; // convert to seconds
+	const deltaTime = now - then; // compute time since last frame
+	then = now; // remember time for next frame
+	const fps = 1 / deltaTime; // compute frames per second
+	fpsElem.textContent = fps.toFixed(1); // update fps display
+
+	// Update loop
+	update(deltaTime);
+	// Render loop
+	render();
+	// Get next frame
+	requestAnimationFrame(draw);
+};
+
+// Draw models onto gl canvas
+var render = function() {
+	// Clear canvas
+	gl.clearColor(0.1, 0.1, 0.1, 1);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	// Transform world matrix
+	world = mat4FromRotTransScale(world, [0, 0, 0], [0, 0, 0], [1, 1, 1]);
+
+	// Draw each light except the tank bomb
+	// TODO improve so that each light has property draw/notdraw
+	lights.forEach(e => {
+		if (e.name != 'bomb' && e.on != 0)
+			e.draw(gl, world, view, proj, lightsJSON);
+	});
+
+	// Draw models
+	tank.draw(gl, world, view, proj, lightsJSON);
+	deer1.draw(gl, world, view, proj, lightsJSON);
+	deer2.draw(gl, world, view, proj, lightsJSON);
+	plane.draw(gl, world, view, proj, lightsJSON);
+};
+
+var update = function(delta) {
+	// Convert all lights to correct format for opengl shader
+	lightsJSON = getLightJSON();
+
+	// Update each light
+	lights.forEach(e => {
+		e.update();
+		// If the light is the one currently selected to move check for keyevents
+		if (lightMove && e.name == lightMove) e.keyboard(delta, keys);
+	});
+
+	// Update tank pass in current key states
+	tank.update(delta, keys);
+
+	// Update deer models
+	// TODO update model method to allow for static models to avoid unneeded updates
+	deer1.update();
+	deer2.update();
+	plane.update();
+
+	// Update camera matrix
+	view = tank.getCamera().getCameraMat();
+};
+
+// Converts lights into format needed for opengl uniform arrays
+var getLightJSON = function() {
+	var c = [];
+	var d = [];
+	var p = [];
+	var on = [];
+
+	lights.forEach(e => {
+		c = c.concat(e.getColour());
+		d = d.concat(e.getDirection());
+		p = p.concat(e.getPosition());
+		on = on.concat(e.getOn());
+	});
+
+	return {
+		c,
+		d,
+		p,
+		on
+	};
+};
+
+// Bind keydown event to window
+window.addEventListener(
+	'keydown',
+	function(e) {
+		keys[e.keyCode] = true;
+		//console.log(e.keyCode);
+	},
+	false
+);
+
+// Bind keyup event to window
+window.addEventListener(
+	'keyup',
+	function(e) {
+		keys[e.keyCode] = false;
+	},
+	false
+);
+
+// TODO Combine with light class
+// Toggles wether the red light should be drawn and used in shader
+$("[name='redLight']").change(function() {
+	if (this.checked) {
+		// If checked draw
+		lights.forEach(e => {
+			if (e.getName() == 'red') {
+				e.setOn(1);
+			}
+		});
+	} else {
+		// Else dont draw
+		lights.forEach(e => {
+			if (e.getName() == 'red') {
+				e.setOn(0);
+			}
+		});
+	}
+	lightsJSON = getLightJSON();
+});
+
+$("[name='blueLight']").change(function() {
+	if (this.checked) {
+		// If checked draw
+		lights.forEach(e => {
+			if (e.getName() == 'blue') {
+				e.setOn(1);
+			}
+		});
+	} else {
+		// Else dont draw
+		lights.forEach(e => {
+			if (e.getName() == 'blue') {
+				e.setOn(0);
+			}
+		});
+	}
+	lightsJSON = getLightJSON();
+});
+
+// Toggles wether the blue light should be moved with the light control keys
+$("[name='blueLightMove']").change(function() {
+	if (this.checked) {
+		// If checked make sure blue movement checkbox is unticked
+		$("[name='redLightMove']").prop('checked', false);
+		// Set light to move to light name
+		lightMove = 'blue';
+	} else {
+		// Else if it is unticked set light to move to null
+		lightMove = null;
+	}
+});
+
+// Toggles wether the red light should be moved with the light control keys
+$("[name='redLightMove']").change(function() {
+	if (this.checked) {
+		// If checked make sure blue movement checkbox is unticked
+		$("[name='blueLightMove']").prop('checked', false);
+		// Set light to move to light name
+		lightMove = 'red';
+	} else {
+		// Else if it is unticked set light to move to null
+		lightMove = null;
+	}
+});
---
+++ javascript/tank.js
@@ -0,0 +1,279 @@
+class Tank {
+	constructor(model) {
+		/*{
+		gl,
+		texture: texture,
+		model: modeljson,
+		shader: shader,
+		rotation: [0, 180, 0],
+		scale: [0.006, 0.006, 0.006],
+		translation: [0, 0, 0],
+		topIndices: [0],
+		botIndices: [1]
+	}*/
+		this.top = new Model({
+			gl: model.gl,
+			texture: model.texture,
+			model: model.model,
+			shader: model.shader,
+			rotation: model.top.rotation,
+			scale: model.top.scale,
+			translation: model.top.translation,
+			meshIndices: model.top.indices
+		});
+		this.bot = new Model({
+			gl: model.gl,
+			texture: model.texture,
+			model: model.model,
+			shader: model.shader,
+			rotation: model.bot.rotation,
+			scale: model.bot.scale,
+			translation: model.bot.translation,
+			meshIndices: model.bot.indices
+		});
+		this.bomb = new Light(
+			{
+				gl: model.gl,
+				texture: model.bomb.texture,
+				model: model.bomb.model,
+				shader: model.shader,
+				rotation: model.bomb.rotation,
+				scale: model.bomb.scale,
+				translation: model.bomb.translation
+			},
+			model.bomb.light
+		);
+
+		this.camera = new Camera({
+			viewPos: [
+				model.top.translation[0] + model.camera.position[0],
+				model.top.translation[1] + model.camera.position[1],
+				model.top.translation[2] + model.camera.position[2]
+			],
+			viewLook: model.top.translation,
+			viewUp: [0, 1, 0]
+		});
+		this.camera.rotateViewPos(model.top.rotation);
+
+		this.turrentSpeed = 60;
+		this.tankRotSpeed = 45;
+		this.tankSpeed = 0.4;
+		this.bombVisible = false;
+		this.bombDirection = [0, 0, 0];
+		this.fired = false;
+		this.bombSpeed = 30;
+		this.bombPos = this.bomb.getTranslation();
+		this.bombRot = this.bomb.getRotation();
+	}
+
+	getCamera() {
+		return this.camera;
+	}
+
+	setCamera(camera) {
+		this.camera = camera;
+	}
+
+	getBomb() {
+		return this.bomb;
+	}
+
+	getTranslation() {
+		return this.bot.getTranslation();
+	}
+
+	getRotation() {
+		return this.bot.getRotation();
+	}
+
+	draw(gl, world, view, proj, lights) {
+		this.top.draw(gl, world, view, proj, lights);
+		this.bot.draw(gl, world, view, proj, lights);
+		if (this.bombVisible) this.bomb.draw(gl, world, view, proj, lights);
+	}
+
+	update(delta, keys) {
+		this.input(delta, keys);
+		this.top.update();
+		this.bot.update();
+		this.bomb.update();
+		if (this.fired) {
+			var trans = this.bomb.getTranslation();
+			this.bomb.setTranslation([
+				trans[0] + delta * this.bombSpeed * this.bombDirection[0],
+				trans[1],
+				trans[2] + delta * this.bombSpeed * this.bombDirection[2]
+			]);
+		}
+	}
+
+	input(delta, keys) {
+		this.rotate(delta, keys);
+		this.move(delta, keys);
+		this.cameraMovement(delta, keys);
+	}
+
+	cameraMovement(delta, keys) {
+		var camrot = 30;
+		var cameraTrans = [0, 0, 0];
+		if (keys['U'.charCodeAt(0)]) {
+			cameraTrans[2] += 0.5 * delta;
+		}
+		if (keys['J'.charCodeAt(0)]) {
+			cameraTrans[2] -= 0.5 * delta;
+		}
+		if (keys['H'.charCodeAt(0)]) {
+			cameraTrans[0] += 0.5 * delta;
+		}
+		if (keys['K'.charCodeAt(0)]) {
+			cameraTrans[0] -= 0.5 * delta;
+		}
+		if (keys['O'.charCodeAt(0)]) {
+			cameraTrans[1] += 0.5 * delta;
+		}
+		if (keys['L'.charCodeAt(0)]) {
+			cameraTrans[1] -= 0.5 * delta;
+		}
+		if (keys['I'.charCodeAt(0)]) {
+			this.camera.rotateViewPos([0, camrot * delta, 0]);
+		}
+		if (keys['Y'.charCodeAt(0)]) {
+			this.camera.rotateViewPos([0, -camrot * delta, 0]);
+		}
+		if (keys['M'.charCodeAt(0)]) {
+			var trans = this.getTranslation();
+			var camera = new Camera({
+				viewPos: [
+					trans[0] + viewPos[0],
+					trans[1] + viewPos[1],
+					trans[2] + viewPos[2]
+				],
+				viewLook: this.getTranslation(),
+				viewUp: [0, 1, 0]
+			});
+			camera.rotateViewPos([0, 0, 0]);
+			camera.rotateViewPos(this.getRotation());
+			this.camera = camera;
+		}
+		this.camera.transCamera(cameraTrans);
+		this.camera.setLookAt(this.getTranslation());
+	}
+
+	rotate(delta, keys) {
+		if (keys['E'.charCodeAt(0)]) {
+			this.rotateModel(delta, -this.turrentSpeed, this.top);
+			this.rotateBomb(delta, -this.turrentSpeed);
+		}
+		if (keys['Q'.charCodeAt(0)]) {
+			this.rotateModel(delta, this.turrentSpeed, this.top);
+			this.rotateBomb(delta, this.turrentSpeed);
+		}
+		if (keys['D'.charCodeAt(0)]) {
+			this.rotateModel(delta, -this.tankRotSpeed, this.top);
+			this.rotateModel(delta, -this.tankRotSpeed, this.bot);
+			this.rotateBomb(delta, -this.tankRotSpeed);
+			this.camera.rotateViewPos([0, delta * -this.tankRotSpeed, 0]);
+		}
+		if (keys['A'.charCodeAt(0)]) {
+			this.rotateModel(delta, this.tankRotSpeed, this.top);
+			this.rotateModel(delta, this.tankRotSpeed, this.bot);
+			this.rotateBomb(delta, this.tankRotSpeed);
+			this.camera.rotateViewPos([0, delta * this.tankRotSpeed, 0]);
+		}
+	}
+
+	rotateBomb(delta, speed) {
+		var tankPos = this.top.getTranslation();
+		vec3.rotateY(
+			this.bombPos,
+			this.bombPos,
+			tankPos,
+			glMatrix.toRadian(speed * delta)
+		);
+
+		this.bombRot[1] = this.bombRot[1] + speed * delta;
+		if (!this.fired) {
+			this.bomb.setRotation([
+				this.bombRot[0],
+				this.bombRot[1],
+				this.bombRot[2]
+			]);
+			this.bomb.setTranslation([
+				this.bombPos[0],
+				this.bombPos[1],
+				this.bombPos[2]
+			]);
+		}
+	}
+
+	rotateModel(delta, speed, model) {
+		var rot = model.getRotation();
+		rot[1] = rot[1] + speed * delta;
+		model.setRotation(rot);
+	}
+
+	move(delta, keys) {
+		if (keys['W'.charCodeAt(0)]) {
+			this.moveModel(delta * this.tankSpeed);
+		}
+		if (keys['S'.charCodeAt(0)]) {
+			this.moveModel(delta * -this.tankSpeed);
+		}
+		if (keys['V'.charCodeAt(0)] && !this.fired) {
+			var rot = this.bomb.getRotation();
+			var forward = [
+				Math.cos(glMatrix.toRadian(rot[1])),
+				0,
+				-Math.sin(glMatrix.toRadian(rot[1]))
+			];
+			vec3.normalize(forward, forward);
+			vec3.mul(
+				forward,
+				[this.tankSpeed, this.tankSpeed, this.tankSpeed],
+				forward
+			);
+			this.bombDirection = forward;
+			this.fired = true;
+			this.bombVisible = true;
+			var that = this;
+			this.bomb.setOn(1);
+			setTimeout(function() {
+				that.bomb.setOn(0);
+				that.bomb.setRotation([
+					that.bombRot[0],
+					that.bombRot[1],
+					that.bombRot[2]
+				]);
+				that.bomb.setTranslation([
+					that.bombPos[0],
+					that.bombPos[1],
+					that.bombPos[2]
+				]);
+				that.fired = false;
+				that.bombVisible = false;
+				that = null;
+			}, 1500);
+		}
+	}
+
+	moveModel(speed) {
+		var rot = this.bot.getRotation();
+		var forward = [
+			Math.sin(glMatrix.toRadian(rot[1])),
+			0,
+			Math.cos(glMatrix.toRadian(rot[1]))
+		];
+		vec3.normalize(forward, forward);
+		vec3.mul(forward, [speed, speed, speed], forward);
+		this.translateModel(forward, this.top);
+		this.translateModel(forward, this.bot);
+		this.camera.transCamera(forward);
+		vec3.add(this.bombPos, this.bombPos, forward);
+	}
+
+	translateModel(forward, model) {
+		var trans = model.getTranslation();
+		vec3.add(trans, trans, forward);
+		model.setTranslation(trans);
+	}
+}
