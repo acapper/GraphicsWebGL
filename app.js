7231ca5469ed3456518c90bf417a380e75f42bfb
Antony Capper <acapper56@gmail.com>
2018-11-01 17:32:29 +0000

    Moved keyboard logic to more appropriate class. Tidied code and added comments.

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var readTextFile = function(file, call) {
-	var rawFile = new XMLHttpRequest();
-	rawFile.open('GET', file, true);
-	rawFile.onreadystatechange = function() {
-		if (rawFile.readyState === 4) {
-			if (rawFile.status === 200 || rawFile.status == 0) {
-				var allText = rawFile.responseText;
-				call(allText);
-			}
-		}
-	};
-	rawFile.send(null);
-};
+const canvas = document.getElementById('mycanvas');
+const fpsElem = document.querySelector('#fps');
 
+var world = new Float32Array(16);
+var view = new Float32Array(16);
+var proj = new Float32Array(16);
+
+var viewPos = [0, 3, -5];
+
+var lights = [];
+var lightsJSON;
+
+var light;
+var tank;
+var deer1;
+var deer2;
+
+var camera;
+var lightMove;
+
+var keys = [];
 var gl;
+
+// Initialise resources needed for opengl scene
 var Init = function() {
+	// Load external resources
 	var proms = loadAll([
 		'texture4.png',
 		'texture2.png',
@@ -27,88 +37,76 @@ var Init = function() {
 		'models/TankShell/TankShell.json'
 	]);
 
-	Promise.all(proms).then(promsR => {
-		var canvas = document.getElementById('mycanvas');
-		gl = canvas.getContext('webgl');
+	// Wait for external resources to load
+	Promise.all(proms)
+		// External resources have loaded
+		.then(promsR => {
+			// Get webgl package
+			gl = canvas.getContext('webgl');
 
-		if (!gl) {
-			gl = canvas.getContext('experimental-webgl');
-		}
+			// If cant get standard webgl package use experimental
+			if (!gl) {
+				gl = canvas.getContext('experimental-webgl');
+			}
 
-		if (!gl) {
-			alert('Your browser does not support WebGL');
-		}
+			// If still cant get webgl package browser doesn't support
+			if (!gl) {
+				alert('Your browser does not support WebGL');
+			}
 
-		$("[name='blueLight']").prop('checked', true);
-		$("[name='blueLightMove']").prop('checked', false);
-		$("[name='redLight']").prop('checked', true);
-		$("[name='redLightMove']").prop('checked', false);
+			// Make sure html checkboxes start in correct state
+			$("[name='blueLight']").prop('checked', true);
+			$("[name='blueLightMove']").prop('checked', false);
+			$("[name='redLight']").prop('checked', true);
+			$("[name='redLightMove']").prop('checked', false);
 
-		gl.clearColor(0, 0, 0, 1);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		gl.enable(gl.DEPTH_TEST);
-		gl.enable(gl.CULL_FACE);
-		gl.frontFace(gl.CCW);
-		gl.cullFace(gl.BACK);
+			// Set gl statemachine values
+			gl.clearColor(0, 0, 0, 1);
+			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+			gl.enable(gl.DEPTH_TEST);
+			gl.enable(gl.CULL_FACE);
+			gl.frontFace(gl.CCW);
+			gl.cullFace(gl.BACK);
 
-		var shader = new Shader(gl, promsR[3], promsR[4]);
-		var lightshader = new Shader(gl, promsR[3], promsR[5]);
-		Run(
-			promsR[0],
-			promsR[1],
-			promsR[2],
-			shader,
-			lightshader,
-			promsR[6],
-			promsR[7],
-			promsR[8],
-			promsR[9]
-		);
-	});
+			// Load and create shaders
+			var shader = new Shader(gl, promsR[3], promsR[4]);
+			var lightshader = new Shader(gl, promsR[3], promsR[5]);
+
+			// Run gl scene
+			Run(
+				promsR[0],
+				promsR[1],
+				promsR[2],
+				shader,
+				lightshader,
+				promsR[6],
+				promsR[7],
+				promsR[8],
+				promsR[9]
+			);
+		})
+		// Catch any errors once the external resources have loaded
+		.catch(err => {
+			throw err;
+		});
 };
 
-var identityMat = new Float32Array(16);
-mat4.identity(identityMat);
-
-var world = new Float32Array(16);
-var view = new Float32Array(16);
-var proj = new Float32Array(16);
-var temp = new Float32Array(16);
-
-var viewPos = [0, 3, -5];
-
-var worldTrans = [0, 0, 0];
-
-var lights = [];
-var redLight;
-var blueLight;
-var lightsJSON;
-
-var light;
-var tank;
-var deer1;
-var deer2;
-var model;
-
-var camera;
-var lightMove;
-
-const fpsElem = document.querySelector('#fps');
-
+// Create and run gl scene
 var Run = function(
-	texture,
-	texture2,
-	texture3,
+	whiteTexture,
+	tankTexture,
+	bulletTexture,
 	shader,
 	lightshader,
-	modeljson,
+	tankjson,
 	deerjson,
 	spherejson,
 	bulletjson
 ) {
+	// Create deer models
 	deer1 = new Model({
 		gl,
-		texture: texture,
+		texture: whiteTexture,
 		model: deerjson,
 		shader: shader,
 		rotation: [-90, 0, 0],
@@ -117,7 +115,7 @@ var Run = function(
 	});
 	deer2 = new Model({
 		gl,
-		texture: texture,
+		texture: whiteTexture,
 		model: deerjson,
 		shader: shader,
 		rotation: [-90, 0, 0],
@@ -125,10 +123,11 @@ var Run = function(
 		translation: [-5, 0, 0]
 	});
 
+	// Create tank model
 	tank = new Tank({
 		gl,
-		texture: texture2,
-		model: modeljson,
+		texture: tankTexture,
+		model: tankjson,
 		shader: shader,
 		top: {
 			rotation: [0, 0, 0],
@@ -143,7 +142,7 @@ var Run = function(
 			indices: [1, 4, 5, 6, 8, 11, 12, 14]
 		},
 		bomb: {
-			texture: texture3,
+			texture: bulletTexture,
 			model: bulletjson,
 			rotation: [0, -90, -90],
 			scale: [0.05, 0.05, 0.05],
@@ -160,10 +159,11 @@ var Run = function(
 		}
 	});
 
+	// Create red light
 	light = new Light(
 		{
 			gl,
-			texture: texture,
+			texture: whiteTexture,
 			model: spherejson,
 			shader: lightshader,
 			rotation: [0, 0, 0],
@@ -172,13 +172,14 @@ var Run = function(
 		},
 		{ name: 'red', colour: [1, 0, 0], direction: [0, 0, 0], on: 1 }
 	);
-
+	// Add light to light tracker
 	lights.push(light);
 
+	// Create blue light
 	light = new Light(
 		{
 			gl,
-			texture: texture,
+			texture: whiteTexture,
 			model: spherejson,
 			shader: lightshader,
 			rotation: [0, 0, 0],
@@ -187,18 +188,29 @@ var Run = function(
 		},
 		{ name: 'blue', colour: [0, 0, 1], direction: [0, 0, 0], on: 1 }
 	);
+	// Add light to light tracker
 	lights.push(light);
 
+	// Add tank bullet light to light tracker
 	lights.push(tank.getBomb());
 
+	// Create world, view and project matrix
 	mat4.identity(world);
 	view = tank.getCamera().getCameraMat();
-	mat4.perspective(proj, glMatrix.toRadian(90), 960 / 720, 0.1, 10000.0);
+	mat4.perspective(
+		proj,
+		glMatrix.toRadian(90),
+		canvas.clientWidth / canvas.clientHeight,
+		0.1,
+		10000.0
+	);
 
+	// Start draw loop
 	requestAnimationFrame(draw);
 };
 
-var then = 0;
+// Web gl main draw loop
+var then = 0; // Keep track of last frame time
 var draw = function(now) {
 	now *= 0.001; // convert to seconds
 	const deltaTime = now - then; // compute time since last frame
@@ -206,138 +218,65 @@ var draw = function(now) {
 	const fps = 1 / deltaTime; // compute frames per second
 	fpsElem.textContent = fps.toFixed(1); // update fps display
 
+	// Update loop
 	update(deltaTime);
-	render(deltaTime);
+	// Render loop
+	render();
+	// Get next frame
 	requestAnimationFrame(draw);
-	now = null;
 };
 
-var rotMat = mat4.create();
-var scaleMat = mat4.create();
-var transMat = mat4.create();
-var render = function(delta) {
+// Draw models onto gl canvas
+var render = function() {
+	// Clear canvas
 	gl.clearColor(0.1, 0.1, 0.1, 1);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+	// Transform world matrix
 	world = mat4FromRotTransScale(world, [0, 0, 0], [0, 0, 0], [1, 1, 1]);
 
+	// Draw each light except the tank bomb
+	// TODO improve so that each light has property draw/notdraw
 	lights.forEach(e => {
 		if (e.name != 'bomb' && e.on != 0)
 			e.draw(gl, world, view, proj, lightsJSON);
 	});
+
+	// Draw models
 	tank.draw(gl, world, view, proj, lightsJSON);
 	deer1.draw(gl, world, view, proj, lightsJSON);
 	deer2.draw(gl, world, view, proj, lightsJSON);
-	delta = null;
 };
 
 var update = function(delta) {
+	// Convert all lights to correct format for opengl shader
 	lightsJSON = getLightJSON();
-	keyboard(delta);
+
+	// Update each light
 	lights.forEach(e => {
 		e.update();
+		// If the light is the one currently selected to move check for keyevents
+		if (lightMove && e.name == lightMove) e.keyboard(delta, keys);
 	});
+
+	// Update tank pass in current key states
 	tank.update(delta, keys);
+
+	// Update deer models
+	// TODO update model method to allow for static models to avoid unneeded updates
 	deer1.update();
 	deer2.update();
-	delta = null;
-};
 
-var camrot = 0;
-
-var keyboard = function(delta) {
-	camrot = 30;
-	var cameraTrans = [0, 0, 0];
-	if (keys['U'.charCodeAt(0)]) {
-		cameraTrans[2] += 0.5 * delta;
-	}
-	if (keys['J'.charCodeAt(0)]) {
-		cameraTrans[2] -= 0.5 * delta;
-	}
-	if (keys['H'.charCodeAt(0)]) {
-		cameraTrans[0] += 0.5 * delta;
-	}
-	if (keys['K'.charCodeAt(0)]) {
-		cameraTrans[0] -= 0.5 * delta;
-	}
-	if (keys['O'.charCodeAt(0)]) {
-		cameraTrans[1] += 0.5 * delta;
-	}
-	if (keys['L'.charCodeAt(0)]) {
-		cameraTrans[1] -= 0.5 * delta;
-	}
-	if (keys['I'.charCodeAt(0)]) {
-		tank.getCamera().rotateViewPos([0, camrot * delta, 0]);
-	}
-	if (keys['Y'.charCodeAt(0)]) {
-		tank.getCamera().rotateViewPos([0, -camrot * delta, 0]);
-	}
-	if (keys['M'.charCodeAt(0)]) {
-		var trans = tank.getTranslation();
-		var camera = new Camera({
-			viewPos: [
-				trans[0] + viewPos[0],
-				trans[1] + viewPos[1],
-				trans[2] + viewPos[2]
-			],
-			viewLook: tank.getTranslation(),
-			viewUp: [0, 1, 0]
-		});
-		camera.rotateViewPos([0, 0, 0]);
-		camera.rotateViewPos(tank.getRotation());
-		tank.setCamera(camera);
-	}
-	tank.getCamera().transCamera(cameraTrans);
-	tank.getCamera().setLookAt(tank.getTranslation());
+	// Update camera matrix
 	view = tank.getCamera().getCameraMat();
-
-	var lightToMove;
-	lights.forEach(e => {
-		if (lightMove && e.name == lightMove) lightToMove = e;
-	});
-	if (lightToMove && lightToMove.getOn() == 1) {
-		if (keys['1'.charCodeAt(0)]) {
-			var pos = lightToMove.getTranslation();
-			pos[2] += 0.5 * delta;
-			lightToMove.getTranslation(pos);
-		}
-		if (keys['2'.charCodeAt(0)]) {
-			var pos = lightToMove.getTranslation();
-			pos[2] -= 0.5 * delta;
-			lightToMove.getTranslation(pos);
-		}
-		if (keys['3'.charCodeAt(0)]) {
-			var pos = lightToMove.getTranslation();
-			pos[0] += 0.5 * delta;
-			lightToMove.getTranslation(pos);
-		}
-		if (keys['4'.charCodeAt(0)]) {
-			var pos = lightToMove.getTranslation();
-			pos[0] -= 0.5 * delta;
-			lightToMove.getTranslation(pos);
-		}
-		if (keys['5'.charCodeAt(0)]) {
-			var pos = lightToMove.getTranslation();
-			pos[1] -= 0.5 * delta;
-			lightToMove.getTranslation(pos);
-		}
-		if (keys['6'.charCodeAt(0)]) {
-			var pos = lightToMove.getTranslation();
-			pos[1] += 0.5 * delta;
-			lightToMove.getTranslation(pos);
-		}
-	}
 };
-var c = [];
-var d = [];
-var p = [];
-var on = [];
 
+// Converts lights into format needed for opengl uniform arrays
 var getLightJSON = function() {
-	c = [];
-	d = [];
-	p = [];
-	on = [];
+	var c = [];
+	var d = [];
+	var p = [];
+	var on = [];
 
 	lights.forEach(e => {
 		c = c.concat(e.getColour());
@@ -354,8 +293,7 @@ var getLightJSON = function() {
 	};
 };
 
-var keys = [];
-
+// Bind keydown event to window
 window.addEventListener(
 	'keydown',
 	function(e) {
@@ -365,6 +303,7 @@ window.addEventListener(
 	false
 );
 
+// Bind keyup event to window
 window.addEventListener(
 	'keyup',
 	function(e) {
@@ -373,14 +312,18 @@ window.addEventListener(
 	false
 );
 
+// TODO Combine with light class
+// Toggles wether the red light should be drawn and used in shader
 $("[name='redLight']").change(function() {
 	if (this.checked) {
+		// If checked draw
 		lights.forEach(e => {
 			if (e.getName() == 'red') {
 				e.setOn(1);
 			}
 		});
 	} else {
+		// Else dont draw
 		lights.forEach(e => {
 			if (e.getName() == 'red') {
 				e.setOn(0);
@@ -392,12 +335,14 @@ $("[name='redLight']").change(function() {
 
 $("[name='blueLight']").change(function() {
 	if (this.checked) {
+		// If checked draw
 		lights.forEach(e => {
 			if (e.getName() == 'blue') {
 				e.setOn(1);
 			}
 		});
 	} else {
+		// Else dont draw
 		lights.forEach(e => {
 			if (e.getName() == 'blue') {
 				e.setOn(0);
@@ -407,20 +352,28 @@ $("[name='blueLight']").change(function() {
 	lightsJSON = getLightJSON();
 });
 
+// Toggles wether the blue light should be moved with the light control keys
 $("[name='blueLightMove']").change(function() {
 	if (this.checked) {
+		// If checked make sure blue movement checkbox is unticked
 		$("[name='redLightMove']").prop('checked', false);
+		// Set light to move to light name
 		lightMove = 'blue';
 	} else {
+		// Else if it is unticked set light to move to null
 		lightMove = null;
 	}
 });
 
+// Toggles wether the red light should be moved with the light control keys
 $("[name='redLightMove']").change(function() {
 	if (this.checked) {
+		// If checked make sure blue movement checkbox is unticked
 		$("[name='blueLightMove']").prop('checked', false);
+		// Set light to move to light name
 		lightMove = 'red';
 	} else {
+		// Else if it is unticked set light to move to null
 		lightMove = null;
 	}
 });
