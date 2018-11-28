30f907c7314f47954b486edb2335aad570221ce6
Antony Capper <acapper56@gmail.com>
2018-11-28 08:54:05 +0000

    Made class to hold a collection of meshes

---
+++ javascript/model.js
@@ -0,0 +1,42 @@
+class Model {
+	constructor(model) {
+		this.meshes = [];
+		for (var i = 0; i < model.meshes.length; i++) {
+			this.meshes.push(
+				new Mesh({
+					gl: model.gl,
+					texture: model.texture,
+					texturescale: model.texturescale,
+					normalmap: model.normalmap,
+					mesh: model.meshes[i],
+					shader: model.shader,
+					rotation: model.rotation,
+					scale: model.scale,
+					translation: model.translation,
+					shininess: model.shininess,
+					emmissive: model.emmissive
+				})
+			);
+		}
+	}
+
+	getMeshes() {
+		return this.meshes;
+	}
+
+	setMeshes(meshes) {
+		this.meshes = meshes;
+	}
+
+	draw(gl, world, view, proj, lightsJSON) {
+		for (var i = 0; i < this.meshes.length; i++) {
+			this.meshes[i].draw(gl, world, view, proj, lightsJSON);
+		}
+	}
+
+	update() {
+		for (var i = 0; i < this.meshes.length; i++) {
+			this.meshes[i].update();
+		}
+	}
+}
